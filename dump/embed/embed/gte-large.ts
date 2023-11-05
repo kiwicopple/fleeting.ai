cd859992f008303c54232bd3d11d2a75856092f7
Copple <10214025+kiwicopple@users.noreply.github.com>
2023-11-05 13:39:02 +0100

    remove the dump now

--- dump/embed/embed/gte-large.ts
+++
@@ -1,6 +0,0 @@
-export default function handler(context) {
-  context.response.headers.set("Content-Type", "application/json");
-  context.response.body = JSON.stringify({
-    "/gte-large": "woo!",
-  });
-}
